04ace1ba1ed83e7891b4315310d5154212dd728b
KeithCissell <keithacissell@gmail.com>
2017-07-03 01:34:49 -0500

    Full test suite constructed with major structural changes

--- src/main/scala/environment/classes/Point.scala
+++ src/main/scala/environment/classes/Point.scala
@@ -3,23 +3,19 @@ package environment.point
 
 import scala.math.sqrt
 import environment.variable._
+import scala.collection.mutable.{ArrayBuffer => AB}
 
-case class Point(val x: Int, val y: Int, var variables: Map[String,Option[Variable]]) {
+case class Point(val x: Int, val y: Int,
+    var variables: AB[Variable] = AB.empty) {
 
-  //        CONSTRUCTOR OVERLOADS
-  def this(x: Int, y: Int, vs: Seq[Variable]) {
-    this(x, y, (vs.map(_.name) zip vs.map(Some(_))).toMap)
-  }
-
-  //        METHODS
   def getX: Int = x
   def getY: Int = y
   def dist(p2: Point): Double = {
     sqrt((p2.getX - x) + (p2.getY - y))
   }
-  def get(varName: String): Option[Variable] = variables.contains(varName) match {
-    case true => variables(varName)
-    case _    => None
+  def get(s: String): Option[Variable] = {
+    var result: Option[Variable] = None
+    for (v <- variables) if (v.name == s) result = Some(v)
+    return result
   }
-  def getAll: Seq[Option[Variable]] = variables.values.toSeq
 }
--- src/test/scala/environment/EnvironmentSpecs.scala
+++ src/test/scala/environment/EnvironmentSpecs.scala
@@ -2,7 +2,9 @@
 import environment._
 import environment.point._
 import environment.variable._
+import environment.GridType.Grid
 import org.specs2.mutable.Specification
+import scala.collection.mutable.{ArrayBuffer => AB, Map => MM}
 
 object EnvironmentSpecs extends Specification {
 
@@ -10,12 +12,53 @@ object EnvironmentSpecs extends Specification {
   ** Create data to test on
   *******************************************************/
 
-  // Variables
-  val height = new Height(Some(417.0))
-  val temperature = new Temperature(77.0)
-  val latitude = new Latitude()
+  // Variable
+  val height1 = new Height(Some(417.0))
+  val latitude1 = new Latitude()
+  val longitude1 = new Longitude(245.5)
+  val temperature1 = new Temperature(77.0)
+  val windSpeed1 = new WindSpeed(0)
+  val variableList1 = AB(
+    height1, latitude1, longitude1, temperature1, windSpeed1
+  )
 
+  val height2 = new Height(17)
+  val latitude2 = new Latitude(23.45)
+  val longitude2 = new Longitude(200.0)
+  val temperature2 = new Temperature(100)
+  val windSpeed2 = new WindSpeed(15)
+  val variableList2 = AB(
+    height2, latitude2, longitude2, temperature2, windSpeed2
+  )
 
+  // Point
+  val point11 = new Point(1, 1, variableList1)
+  val point12 = new Point(1, 2, variableList2)
+  val point13 = new Point(1, 3)
+  val point21 = new Point(2, 1)
+  val point22 = new Point(2, 2)
+  val point23 = new Point(2, 3)
+  val point31 = new Point(3, 1)
+  val point32 = new Point(3, 2)
+  val point33 = new Point(3, 3)
+
+  // Environment
+  val row1: AB[Option[Point]] = AB(Some(point11), Some(point12), Some(point13))
+  val row2: AB[Option[Point]] = AB(Some(point21), Some(point22), Some(point23))
+  val row3: AB[Option[Point]] = AB(Some(point31), Some(point32), Some(point33))
+  val grid: Grid[Point] = AB(row1, row2, row3)
+  val environment = new Environment("Test", grid)
+  val emptyEnv = new Environment("Empty")
+
+  // Other
+  val allVariableTypes = AB(
+    height1, latitude1, longitude1, temperature1, windSpeed1
+  )
+  val testLayer = AB(
+    AB(Some(height1), Some(height2), None),
+    AB(None, None, None),
+    AB(None, None, None)
+  )
 
 
 
@@ -27,18 +70,56 @@ object EnvironmentSpecs extends Specification {
   "\nVariable classes hold environmental information and" should {
 
     "Properly construct" in {
-      (height.value == Some(417.0)) &&
-      (temperature.value == Some(77.0)) &&
-      (latitude.value == None)
+      (height1.value == Some(417.0)) &&
+      (temperature1.value == Some(77.0)) &&
+      (latitude1.value == None)
     }
-    step(height.set(0.0))
-    step(temperature.set(85.0))
-    step(latitude.set(123.45))
-    "Allow inconstant or uninitialized value to be set" in {
-      (height.value == Some(417.0)) &&
-      (temperature.value == Some(85.0)) &&
-      (latitude.value == Some(123.45))
+    step(height1.set(0.0))
+    step(temperature1.set(85.0))
+    step(latitude1.set(123.45))
+    "Allow uninitialized or inconstant value to be set" in {
+      (height1.value == Some(417.0)) &&
+      (temperature1.value == Some(85.0)) &&
+      (latitude1.value == Some(123.45))
     }
   }
+  // Point Tests
+  "\nPoint class holds (x,y) position and list of Variables and" should {
+
+    "Properly construct" in {
+      (point11.variables == variableList1) &&
+      (point13.variables == AB.empty)
+    }
+    "Get x position" in {
+      point11.getX == 1
+    }
+    "Get y position" in {
+      point11.getY == 1
+    }
+    "Calculate distance to another Point" in {
+      point11.dist(point12) == 1.0
+    }
+  }
+  // Environment Tests
+  "\nEnvironment class holds a grid of Points and" should {
+
+    "Properly construct" in {
+      (environment.grid == grid) &&
+      (emptyEnv.grid == AB(AB(None)))
+    }
+    "Return a point from (x,y)" in {
+      environment.getPoint(1, 1) == Some(point11)
+    }
+    "Return a Set of all varialbe names on the grid" in {
+      environment.getVariableNames == allVariableTypes.map(_.name).toSet
+    }
+    "Return a grid layer of a specific Variable" in {
+      environment.getLayer("Height") == testLayer
+    }
+  }
+
+  // step(println(s"\n\n${}\n\n"))
+  // step(println(s"\n\n${}\n\n"))
+
 
 }
