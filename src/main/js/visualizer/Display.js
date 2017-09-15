0061d1db0c5be4f8cb8e082efccd42d0a0305a06
KeithCissell <keithacissell@gmail.com>
2017-09-15 01:12:52 -0500

    Updated directory organization. Messed with CSS

--- src/main/js/visualizer/Display.js
+++ src/main/js/visualizer/Display.js
@@ -1,63 +1,11 @@
+// External Libraries
 const d3 = require('d3')
 const d3Contour = require('d3-contour')
 const hsv = require('d3-hsv')
 
-// Document Elements
-const header = document.getElementById("header")
-const toolbar = document.getElementById("toolbar")
-const currentLayerName = document.getElementById("current-layer-name")
-const main = document.getElementById("main")
-const message = document.getElementById("message")
-const mainContent = document.getElementById("content")
 
 // Globals
-let environment
-let elementTypes
-let currentLayerIndex = 0
-
-function loadEnvironmentDisplay(targetEnvironment) {
-  environment = targetEnvironment
-  elementTypes = environment.elementTypes
-  loadToolbar(elementTypes)
-  displayLayer(currentLayerIndex)
-}
-
-function loadToolbar(elementTypes) {
-  let previousLayerButton = document.createElement("button")
-  previousLayerButton.textContent = " <<< "
-  previousLayerButton.addEventListener("click", () => {
-    switchLayer(currentLayerIndex - 1)
-  })
-  toolbar.insertBefore(previousLayerButton, currentLayerName)
-  let nextLayerButton = document.createElement("button")
-  nextLayerButton.textContent = " >>> "
-  nextLayerButton.addEventListener("click", () => {
-    switchLayer(currentLayerIndex + 1)
-  })
-  toolbar.appendChild(nextLayerButton)
-  for (let e in elementTypes) {
-    let elementType = elementTypes[e]
-
-  }
-}
-
-function switchLayer(newIndex) {
-  if (newIndex < 0) displayLayer(elementTypes.length - 1)
-  else if (newIndex >= elementTypes.length) displayLayer(0)
-  else displayLayer(newIndex)
-}
-
-function displayLayer(index) {
-  currentLayerIndex = index
-  let elementType = elementTypes[index]
-  let layer = environment.extractLayer(elementType)
-  currentLayerName.innerText = layer.elementType
-  message.innerHTML = ""
-
-  if (elementType == "Elevation") drawContourPlot(layer)
-  else drawCanvas(layer)
-}
-
+const canvas = d3.select("#canvas")
 
 function drawCanvas(layer) {
   console.log(layer)
@@ -76,11 +24,6 @@ function drawCanvas(layer) {
               .domain([min, max])
               //.range(["purple", "red"])
 
-  let canvas = d3.select("#canvas")
-      .attr("width", width)
-      .attr("height", height)
-  // console.log(canvas)
-
   let context = canvas.node().getContext("2d")
   let image = context.createImageData(width, height)
 
@@ -91,9 +34,9 @@ function drawCanvas(layer) {
     image.data[i * 4 + 2] = c.b
     image.data[i * 4 + 3] = 255
   }
-
   context.putImageData(image, 0, 0)
 }
+
 
 function drawContourPlot(layer) {
   console.log(layer)
@@ -111,10 +54,10 @@ function drawContourPlot(layer) {
   let interpolateTerrain = function(t) { return t < 0.5 ? i0(t * 2) : i1((t - 0.5) * 2) }
   let color = d3.scaleSequential(interpolateTerrain).domain([min, max])
 
-  let canvas = d3.select("#canvas")
-      .style("background-color", "white")
-      .attr("width", 500)
-      .attr("height", 500)
+  // let canvas = d3.select("#canvas")
+  //     .style("background-color", "white")
+  //     .attr("width", 500)
+  //     .attr("height", 500)
 
   let contours = d3Contour.contours()
       .size([width, height])
@@ -150,4 +93,4 @@ function layerToJson(layer) {
 }
 
 
-export {loadEnvironmentDisplay}
+export {drawCanvas, drawContourPlot}
